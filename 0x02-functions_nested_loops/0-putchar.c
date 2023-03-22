29ee3b17b7a562f6ea6b157a61f7a2408157077e
Nel-tech <favournelson82@gmail.com>
2023-03-22 12:52:12 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "Favour.Lee"
+
+/**
+ * main - prints FavourLee
+ *
+ * Description: prints FavourLee
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "FavourLee";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
